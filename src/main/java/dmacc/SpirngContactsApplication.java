e6d1529503f20c14e360390a55609f2d4571da20
McHammer18 <65956506+McHammer18@users.noreply.github.com>
2020-10-26 16:22:13 -0500

    update
    added web conntroller with web pages that interact with it

--- src/main/java/dmacc/SpirngContactsApplication.java
+++ src/main/java/dmacc/SpirngContactsApplication.java
@@ -16,37 +16,37 @@ import dmacc.controller.BeanConfiguration;
 import dmacc.repository.ContactRepository;
 
 @SpringBootApplication
-public class SpirngContactsApplication implements CommandLineRunner{
+public class SpirngContactsApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpirngContactsApplication.class, args);
 	}
-		@Autowired
-		ContactRepository repo;
+		//@Autowired
+		//ContactRepository repo;
 		
-		@Override
-		public void run(String... args) throws Exception
-		{
-		ApplicationContext appContext = new AnnotationConfigApplicationContext(BeanConfiguration.class);
+		//@Override
+		//public void run(String... args) throws Exception
+		//{
+		//ApplicationContext appContext = new AnnotationConfigApplicationContext(BeanConfiguration.class);
 		
 		//Using an existing bean
-		Contact c = appContext.getBean("contact", Contact.class);
-		c.setRelationship("best fried");
-		repo.save(c);
+		//Contact c = appContext.getBean("contact", Contact.class);
+		//c.setRelationship("best fried");
+		//repo.save(c);
 		
 		//Creating a bean to use - not managed by spring
-		Contact d = new Contact("Sandra Boyton", "555-498-8877", "friend");
-		Address a = new Address("123 Main Street", "Ankeny", "IA");
-		d.setAddress(a);
-		repo.save(d);
+		//Contact d = new Contact("Sandra Boyton", "555-498-8877", "friend");
+		//Address a = new Address("123 Main Street", "Ankeny", "IA");
+		//d.setAddress(a);
+		//repo.save(d);
 		
-		List<Contact> allMyContacts = repo.findAll();
-		for(Contact people: allMyContacts)
-		{
-			System.out.println(people.toString());
-		}
+		//List<Contact> allMyContacts = repo.findAll();
+		//for(Contact people: allMyContacts)
+		//{
+		//	System.out.println(people.toString());
+		//}
 		
-		((AbstractApplicationContext) appContext).close();
+		//((AbstractApplicationContext) appContext).close();
 		
-		}
+		//}
 }
